e6fbae4e53ba99ad5f558aa16c404e747a29f71c
krunal <krunalpabari13>
2023-08-18 01:28:18 +0530

    first

--- components/Footer.js
+++ components/Footer.js
@@ -64,7 +64,7 @@ const Footer = () => {
         <Row className="mt-5">
           <Col md={12}>
             <div className="text-center text-muted">
-              <p className="mb-0 f-15">2020 © Name. Design by Name</p>
+             
             </div>
           </Col>
         </Row>
